933df865e156af3ca43f9f45d7504e7481898416
EBassettHound <ebass227@gmail.com>
2023-10-15 20:43:55 -0400

    all tests passing

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,128 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+function list(req, res, next) {
+    res.send({ data: dishes })
+}
+
+function postValidator(req, res, next) {
+    const post = req.body.data
+    if (!("name" in post)) {
+        next({
+            status: 400,
+            message:"Dish must include a name"})
+    }
+    if (post.name === "") {
+        next({
+            status: 400,
+            message:"Dish must include a name"})
+    }
+    if (!("description" in post)) {
+        next({
+            status: 400,
+            message:"Dish must include a description"})
+    }
+    if (post.description === "") {
+        next({
+            status: 400,
+            message:"Dish must include a description"})
+    }
+    if (!("image_url" in post)) {
+        next({
+            status: 400,
+            message:"Dish must include a image_url"})
+    }
+    if (post.image_url === "") {
+        next({
+            status: 400,
+            message:"Dish must include a image_url"})
+    }
+    if (!("price" in post)) {
+        next({
+            status: 400,
+            message:"Dish must include a price"})
+    }
+    if (typeof post.price !== "number") {
+        next({
+            status: 400,
+            message:"Dish must have a price that is an integer greater than 0"})
+    }
+    if (post.price <= 0) {
+        next({
+            status: 400,
+            message:"Dish must have a price that is an integer greater than 0"})
+    }
+    else {
+        next();
+    }
+
+}
+
+function create(req, res, next) {
+   const newDish = {
+        "id" : nextId(),
+        "name": req.body.data.name,
+        "description": req.body.data.description,
+        "price": req.body.data.price,
+        "image_url": req.body.data.image_url
+    }
+    dishes.push(newDish);
+    res.status(201).send({ data: newDish })
+}
+function read(req, res, next) {
+    const foundDish = dishes.find(dish=>dish.id === req.params.dishId)
+    
+    if (foundDish !== undefined) {
+        res.status(200).send({ data: foundDish })
+    }
+    else {
+        next({
+            status: 404,
+            })
+
+    }
+}
+function validateDishExists(request, response, next) {
+    const { dishId } = request.params;
+    const foundDish = dishes.find((dish) => dish.id === dishId);
+    if (foundDish) {
+        response.locals.dish = foundDish;
+        return next();
+    }
+    next({
+        status: 404,
+        message: `Dish id is not found: ${dishId}`,
+    })
+};
+function validateDishId(request, response, next) {
+    const { dishId } = request.params;
+    const { data: { id } = {} } = request.body;
+    if (!id || id === dishId ) {
+        response.locals.dishId = dishId;
+        return next();
+    }
+    next({
+        status: 400,
+        message: `Dish id does not match route id. Dish: ${id}. Route: ${dishId}.`
+    });
+};
+
+function update(req, res) {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+    res.locals.dish = {
+      id: res.locals.dishId,
+      name: name,
+      description: description,
+      price: price,
+      image_url: image_url,
+    };
+    res.json({ data: res.locals.dish });
+  };
+
+module.exports = {
+    list,
+    create : [postValidator, create],
+    read,
+    update: [validateDishExists, postValidator, validateDishId, update]
+}
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,129 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+function validateOrderExists(req, res, next) {
+    const { orderId } = req.params;
+    const foundOrder = orders.find((order) => order.id === orderId);
+    if (foundOrder) {
+      res.locals.order = foundOrder;
+      return next();
+    }
+    next({
+      status: 404,
+      message: `Order id does not exist: ${orderId}`,
+    });
+}
+
+function validateOrderBody(req, res, next) {
+    const { data: { deliverTo, mobileNumber, dishes } = {} } = req.body;   
+    if (!deliverTo || deliverTo === "") next({ status: 400, message: "Order must include a deliverTo" });
+    if (!mobileNumber || mobileNumber === "") next({ status: 400, message: "Order must include a mobileNumber" }); 
+    if (!dishes) next({ status: 400, message: "Order must include at least one dish" });
+    if (!Array.isArray(dishes) || dishes.length === 0) {
+      return next({
+        status: 400,
+        message: "Order must include at least one dish",
+      });
+    }
+
+    dishes.map((dish, index) => {
+      if (
+          !dish.quantity ||
+          !Number.isInteger(dish.quantity) ||
+          !dish.quantity > 0
+        ) {
+        return next({
+          status: 400,
+          message: `Dish ${index} must have a quantity that is an integer greater than 0.`,
+        });
+      }
+    });
+    res.locals.order = req.body.data;
+    next();
+}
+
+function validateDestroy(req, res, next) {
+    if(res.locals.order.status !== "pending") {
+        return next({
+            status: 400,
+            message: "An order cannot be deleted unless it is pending",
+        });
+    }
+    next();
+};
+
+function validateStatus(req, res, next) {
+    const { orderId } = req.params;
+      const { data: { id, status } = {} } = req.body;
+  
+      if(id && id !== orderId) {
+      return next({
+        status: 400,
+        message: `Order id does not match route id. Order: ${id}, Route: ${orderId}`
+      })
+    }
+      else if(!status || status === "" || (status !== "pending" && status !== "preparing" && status !== "out-for-delivery")) {
+      return next({
+        status: 400,
+        message: "Order must have a status of pending, preparing, out-for-delivery, delivered"
+      })
+    }		
+
+      else if(status === "delivered"){
+      return next({
+        status: 400,
+        message: "A delivered order cannot be changed"
+      })
+    }
+    next();
+};
+
+
+/* Middleware */
+function list(req, res) {
+    res.json({ data: orders });
+}
+
+function create(req, res) {
+    const { data: { deliverTo, mobileNumber, status, dishes} = {} } = req.body;
+    const newOrder = {
+        id: nextId(),
+        deliverTo: deliverTo,
+        mobileNumber: mobileNumber,
+        status: status,
+        dishes: dishes,
+    };
+    orders.push(newOrder);
+    res.status(201).json({ data: newOrder });
+};
+
+function read(req, res) {
+    res.json({ data: res.locals.order });
+}
+
+function update(req, res) {
+    const { data: { deliverTo, mobileNumber, dishes, status } = {} } = req.body;
+    res.locals.order = {
+        id: res.locals.order.id,
+        deliverTo: deliverTo,
+        mobileNumber: mobileNumber,
+        dishes: dishes,
+        status: status,
+    };
+    res.json({ data: res.locals.order });
+};
+  
+
+function destroy(req, res) {
+    const index = orders.indexOf(res.locals.order);
+    orders.splice(index, 1);
+    res.sendStatus(204);
+}
+
+module.exports = {
+    list,
+    create: [validateOrderBody, create],
+    read: [validateOrderExists, read],
+    update: [validateOrderBody, validateOrderExists, validateStatus, update],
+    delete: [validateOrderExists, validateDestroy, destroy],
+};
